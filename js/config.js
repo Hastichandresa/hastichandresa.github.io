344d3eb6ab8edff6bed4053743706edd40a909f7
Hasti <152002788+Hastichandresa@users.noreply.github.com>
2025-09-18 22:09:59 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,9 +9,9 @@ window.SITE_CONFIG = {
     phone: "+197 543 2345",
     profile: {
         name: "Hasti",
-        roles: ["screenwriter", "bakchod"],
-        location: "Indore",
-        introPretitle: "YO YO"
+        roles: ["screenwriter", ""],
+        location: "",
+        introPretitle: ""
     },
     about: {
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
